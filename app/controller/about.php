2a6c40cf45ff243cccb06e55fe41064df7b18023
MuzzamilQureshi007 <62890150+MuzzamilQureshi007@users.noreply.github.com>
2021-01-12 15:45:49 +0500

    view product page ui
    view product page ui

--- app/controller/about.php
+++ app/controller/about.php
@@ -9,6 +9,10 @@ $PATH =  constant("APPLICATION_INNERPATH");
 require $PATH . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'service_config.php'; 
 $DB_CLASS =  $config_service['DB_CLASS'];
 require_once $config_service['FUNCTIONS'];
+require_once $DB_CLASS;
+$query;
+$db;	
+$varr = new databaseManager();
 
 /* about description */
 $formDesc       = $_POST['formDesc']        = isset($_POST['formDesc']) ? $_POST['formDesc'] : '';
@@ -24,5 +28,23 @@ $contact_email  = $_POST['contact_email']   = isset($_POST['contact_email']) ? $
 /* vision */
 $visionForm     = $_POST['visionForm']      = isset($_POST['visionForm']) ? $_POST['visionForm'] : '';
 $vision         = $_POST['vision']          = isset($_POST['vision']) ? $_POST['vision'] : '';
-echo '1';
+
+
+
+if(!empty($about_desc)){
+
+    $varr->query="UPDATE  `config` SET value=? where name = 'AboutDiscribtion' ";
+    $result=$varr->executeQuery($varr->query,array($about_desc),"update");
+
+$response = 1;
+}
+
+else{
+
+
+    $response = 0;
+
+}
+echo $response;
+
 ?>
