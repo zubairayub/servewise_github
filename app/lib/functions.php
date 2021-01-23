dc4a0bbcd36bfacb8528dc2944ba04006b56d883
zubairayub <zubairayub@hotmail.com>
2021-01-23 17:44:58 +0500

    Update functions.php

--- app/lib/functions.php
+++ app/lib/functions.php
@@ -8,8 +8,8 @@ function get($name, $def= '')
 	 return isset($_REQUEST[$name]) ? $_REQUEST[$name] : $def;
 }
 
-echo sendEmail('zubairayub@hotmail.com','servewise','zubx2010@hotmail.com','hello world','Registration');
-exit();
+// echo sendEmail('zubairayub@hotmail.com','servewise','zubx2010@hotmail.com','hello world','Registration');
+// exit();
 
 function getnotifications($dbclass,$userid,$type = NULL){
 
