74e9fee1c7b7ebffd4ecfd7178d0b5e058316c29
zubairayub <zubairayub@hotmail.com>
2021-01-25 18:47:28 +0500

    Update functions.php

--- app/lib/functions.php
+++ app/lib/functions.php
@@ -252,7 +252,7 @@ if (ini_get("session.use_cookies")) {
     // clear cookies and sessions
     setcookie(session_name(), '', time() - 42000,
         $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
+        $params["secure"], $params["httpsonly"], $params["httponly"]
     );
 }
 // Just in case.. swipe these values too
