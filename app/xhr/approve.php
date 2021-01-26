c527269372af14c36c0ac9d70224eb004d633ea4
zubairayub <zubairayub@hotmail.com>
2021-01-26 18:30:40 +0500

    Update approve.php

--- app/xhr/approve.php
+++ app/xhr/approve.php
@@ -17,14 +17,59 @@ $type       =   $_GET['type'];
 $action     =   $_GET['action'];
 $user_id    =   $_GET['user_id'];
 $branch_id    =   $_GET['branch_id'];
-
+$user_info = getuserinfo($user_id);
+$email = $user_info[0]['email_id'];
 if($type == 'vendor'){
 $result = approve_vendor($user_id,$vendor_id,$action,$DB_CLASS);
+
+if($action == 'approve'){
+
+			$message_body = 'Congratulations! Your Request for become vendor has been approved. Thanks for becoming a part of ServeWise';
+			$subject = 'Shop Approved';
+
+
 }else{
+
+			$message_body = 'Hello your shop has been delisted from ServeWise. for more queries please contact at info@servewise.shop Thanks';
+			$subject = 'Shop Delisted';
+
+
+}
+
+}else{
+
+
+
 
 $result = approve_branch($user_id,$vendor_id,$branch_id,$action,$DB_CLASS);
 
+
+if($action == 'approve'){
+
+			$message_body = 'Congratulations! Your Request for Branch has been approved. Thanks for becoming a part of ServeWise';
+			$subject = 'Shop_Approved';
+
+
+}else{
+
+			$message_body = 'Hello your shop has been delisted from ServeWise. for more queries please contact at info@servewise.shop Thanks';
+			$subject = 'Shop_Delisted';
+
+
 }
+
+
+}
+
+
+
+$from_email = 'vendor@servewise.shop';
+			 sendEmail($email,'ServeWise',$from_email,$message_body,$subject);
+			
+			// insert_notifications($DB_CLASS,$user_id,'6','vendor_singup','https://servewise.shop');
+			
+			 insert_notifications($DB_CLASS,'6',$user_id,$subject,'https://servewise.shop');
+
 
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
