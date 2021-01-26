dccb3f7690e20c6ad18c709d8d581b0a90f6711b
zubairayub <zubairayub@hotmail.com>
2021-01-26 17:29:56 +0500

    Update signup.php

--- app/controller/signup.php
+++ app/controller/signup.php
@@ -60,7 +60,7 @@ mail($to,$subject,$txt,$headers);
 		  $signup= 1;
 		   
 		if (!empty($signup)){
-			$from_email = 'register@servewise.shop' ;
+			$from_email = 'register@servewise.com' ;
 			$message_body = 'Welcome to ServeWise You securtiy code is '.$security_code.' Please verify your email using this code Thanks';
 			 echo sendEmail($email,'ServeWise',$from_email,$message_body,'Registration');
 			// insert_notifications($DB_CLASS,'6','6','User_register','https://servewise.shop');
