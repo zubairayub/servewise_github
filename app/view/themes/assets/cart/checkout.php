fa42adf0f7c63b59b3470e4bad3e6e572fa6e8ed
anarshah <javacodesmaster@gmail.com>
2021-01-27 18:52:45 +0500

    checkout
    modification

--- app/view/themes/assets/cart/checkout.php
+++ app/view/themes/assets/cart/checkout.php
@@ -49,14 +49,52 @@ endforeach;
 <?php  include '../themePages/theme_header.phtml'; 
 
 foreach($array as $key => $value): for($i=0; $i < $count; $i++) :?>
-	<aside>
-		<div class="cart_summary"> 
-			<p>Product Name: <?= $value['product_name']; ?></p> 
-			<p>Subtotal: <?= $value['subtotal']; ?></p>
-			<p>Discount:</p>
-			<p>Payment Method: <?= $payment_method;?></p>
-		</div>
-	</aside><?php endfor; ?>
+	<div class="container">
+	<table id="cart" class="table table-hover table-condensed">
+    				<thead>
+						<tr>
+							<th style="width:50%">Product</th>
+							<th style="width:10%">Price</th>
+							<th style="width:8%">Quantity</th>
+							<th style="width:22%" class="text-center">Subtotal</th>
+							<th style="width:10%"></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td data-th="Product">
+								<div class="row">
+									<div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
+									<div class="col-sm-10">
+										<h4 class="nomargin"><?= $value['product_name'][$i]; ?> 1</h4>
+										<!-- <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p> -->
+									</div>
+								</div>
+							</td>
+							<td data-th="Price">$1.99</td>
+							<td data-th="Quantity">
+								<input type="number" class="form-control text-center" value="1">
+							</td>
+							<td data-th="Subtotal" class="text-center">1.99</td>
+							<td class="actions" data-th="">
+								<button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
+								<button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
+							</td>
+						</tr>
+					</tbody>
+					<tfoot>
+						<tr class="visible-xs">
+							<td class="text-center"><strong>Total 1.99</strong></td>
+						</tr>
+						<tr>
+							<td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
+							<td colspan="2" class="hidden-xs"></td>
+							<td class="hidden-xs text-center"><strong>Total $1.99</strong></td>
+							<td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
+						</tr>
+					</tfoot>
+				</table>
+</div><?php endfor; ?>
 <div class="container checkout-main">
 <h3>Payment Form</h3>
 <form id="js-demo-form" name="form" method="post">
