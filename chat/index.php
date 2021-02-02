bc709f5cc137a660e54dd6aaa441207427309194
zubairayub <zubairayub@hotmail.com>
2021-02-02 15:35:04 +0500

    update with table

--- chat/index.php
+++ chat/index.php
@@ -58,12 +58,7 @@ function loginForm(){
             </div>
  
             <div id="chatbox">
-            <?php
-            if(file_exists("log.html") && filesize("log.html") > 0){
-                $contents = file_get_contents("log.html");          
-                echo $contents;
-            }
-            ?>
+            Coversations
             </div>
  
             <form name="message" action="">
