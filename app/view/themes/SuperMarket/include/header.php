f384a35b2df672ff234f4ed29610bba16aa0c8c1
zubairayub <zubairayub@hotmail.com>
2021-02-08 17:30:14 +0500

    Update header.php

--- app/view/themes/SuperMarket/include/header.php
+++ app/view/themes/SuperMarket/include/header.php
@@ -8,4 +8,13 @@ include '../include/config.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="css/supermarket.css">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HSMSFGFNM7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HSMSFGFNM7');
+</script>
 </head>
