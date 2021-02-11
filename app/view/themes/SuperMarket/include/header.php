b9d18e018fee547a209dcbe05c051cb809b64e99
zubairayub <zubairayub@hotmail.com>
2021-02-11 20:06:50 +0500

    Update header.php

--- app/view/themes/SuperMarket/include/header.php
+++ app/view/themes/SuperMarket/include/header.php
@@ -4,7 +4,7 @@ include '../include/config.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SuperMarket</title>
+    <title><?= $branch_name ;?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="css/supermarket.css">
