1a53dd6add572d7d8521dd1d515605e0c7277e0d
anarshah <javacodesmaster@gmail.com>
2021-02-18 17:10:49 +0500

    product id
    for analytics

--- app/view/themes/SuperMarket/supermarket.php
+++ app/view/themes/SuperMarket/supermarket.php
@@ -273,7 +273,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                     <div class="product-card-button item">
                         <input name="product_price" value="<?= $value['price']?>" type="hidden" />
                         <input name="product_id" value="<?= $value['product_id'] ?>" type="hidden" />
-                        <a href="#" class="sc-add-to-cart add-to-cart">Add to Cart<i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+                        <a href="#" class="sc-add-to-cart add-to-cart" id="<?= $value['name']; ?>">Add to Cart<i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
                     </div>
                 </div>
                <?php
@@ -297,7 +297,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                     <div class="product-card-button">
                         <input name="product_price" value="<?= $value['price']?>" type="hidden" />
                         <input name="product_id" value="<?= $value['product_id'] ?>" type="hidden" />
-                        <a href="#" class="sc-add-to-cart add-to-cart">Add to Cart<i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+                        <a href="#" class="sc-add-to-cart add-to-cart" id="<?= $value['name']; ?>">Add to Cart<i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
                     </div>
                 </div>
             </div>
