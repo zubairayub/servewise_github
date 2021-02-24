34e3f45252315b35b750e0c7b0d4a41ce0e09519
zubairayub <zubairayub@hotmail.com>
2021-02-24 17:33:35 +0500

    Update checkoutData.php

--- app/view/themes/assets/cart/checkoutData.php
+++ app/view/themes/assets/cart/checkoutData.php
@@ -3,12 +3,10 @@ REQUIRE '../../include/config.php';
 
 if(!empty($_POST)){
 
-$order_id =  process_order($DB_CLASS,$_POST,$owner_id,$vb_id);
-$_SESSION['order_id'] = $order_id;
+echo process_order($DB_CLASS,$_POST,$owner_id,$vb_id);
+
 }
 if(!empty($_GET)){
-
-	echo $_SESSION['order_id'];
     echo $_GET['status'].'<br>';
     echo $_GET['transaction_id'].'<br>';
 }
