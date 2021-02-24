e46567c860aaab52647857d1c7f9b30c4bf67e8d
anarshah <javacodesmaster@gmail.com>
2021-02-24 16:12:32 +0500

    checkout setting

--- app/view/themes/assets/cart/checkoutData.php
+++ app/view/themes/assets/cart/checkoutData.php
@@ -5,13 +5,13 @@ if(!empty($_POST)){
 
 echo process_order($DB_CLASS,$_POST,$owner_id,$vb_id);
 
-
-
-
-
-
-
 }
+if(!empty($_GET)){
+    echo $_GET['status'];
+    echo $_GET['transaction_id'];
+    echo process_order($DB_CLASS,$_POST,$owner_id,$vb_id);
+}
+
 
 
 ?>
