4e17a5c301cb07fc2adece31f0db8383c253c2d6
zubairayub <zubairayub@hotmail.com>
2021-03-02 20:10:51 +0500

    dashboard logo fixed by vendor

--- app/lib/functions.php
+++ app/lib/functions.php
@@ -798,7 +798,7 @@ if(!empty($dbclass)){
 
 
 
-function getlogo($dbclass = null,$directory = null){
+function getlogo($dbclass = null,$directory = null,$vendor_id = null){
 
 if(!empty($dbclass)){
 		
@@ -809,8 +809,11 @@ if(!empty($dbclass)){
 	$db;
 	$varr = new databaseManager();
 
-	
+	if(empty($vendor_id)){
 		$varr->query = "SELECT * FROM `config` where name='logo'  AND userid= 6 ";
+	}else{
+$varr->query = "SELECT * FROM `config` where name='logo'  AND userid= $vendor_id ";
+	}
 	
 	$result = $varr->executeQuery($varr->query,array(),"sread");
 
