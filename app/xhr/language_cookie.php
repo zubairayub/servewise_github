afdf08c53a0e714b0d72e3b3406db815f787592a
zubairayub <zubairayub@hotmail.com>
2021-03-06 00:17:34 +0500

    Update language_cookie.php

--- app/xhr/language_cookie.php
+++ app/xhr/language_cookie.php
@@ -1,39 +1,43 @@
 <?php
 
 
-session_start();
+// session_start();
 
-defined('APPLICATION_INNERPATH') || define('APPLICATION_INNERPATH', realpath( dirname(__FILE__) . '/../'));
-
-
-$PATH =  constant("APPLICATION_INNERPATH");
-
-require $PATH . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'service_config.php'; 
-$DB_CLASS =  $config_service['DB_CLASS'];
-require_once $config_service['FUNCTIONS'];
+// defined('APPLICATION_INNERPATH') || define('APPLICATION_INNERPATH', realpath( dirname(__FILE__) . '/../'));
 
 
+// $PATH =  constant("APPLICATION_INNERPATH");
+
+// require $PATH . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'service_config.php'; 
+// $DB_CLASS =  $config_service['DB_CLASS'];
+// require_once $config_service['FUNCTIONS'];
 
 
-if(!empty($_GET['lng'])){
-
-	$LANG = $_GET['lng'];
-
-setlangcookie($LANG);
 
 
-$returnpage = $_SERVER['HTTP_REFERER'] ;
+// if(!empty($_GET['lng'])){
+
+// 	$LANG = $_GET['lng'];
+
+// setlangcookie($LANG);
 
 
-    //header("Location: $returnpage");
+// $returnpage = $_SERVER['HTTP_REFERER'] ;
 
 
-}
+//     //header("Location: $returnpage");
 
+
+// }
+
+
+$value = 'something from somewhere';
+
+setcookie("TestCookie", $value);
 
 ?>
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 	window.location.replace("<?= $returnpage ?>");
 
-</script>
+</script> -->
