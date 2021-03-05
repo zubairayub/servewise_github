3ef576c091a244b5905f53ce34b811ab3c5a2a3f
zubairayub <zubairayub@hotmail.com>
2021-03-06 01:22:57 +0500

    Update language_cookie.php

--- app/xhr/language_cookie.php
+++ app/xhr/language_cookie.php
@@ -53,7 +53,7 @@ $returnpage = $_SERVER['HTTP_REFERER'] ;
 
 ?>
 
-<!-- <script type="text/javascript">
+<script type="text/javascript">
 	window.location.replace("<?= $returnpage ?>");
 
-</script> -->
+</script>
