3ed298580f0a70eabcbe03670374c2c3abeb8240
Rasheed Ur Rehman <rasheed1612e@users.noreply.github.com>
2021-03-25 19:16:24 +0500

    savein excel file

---
+++ app/model/subscribers.php
@@ -0,0 +1,14 @@
+<?php  
+
+defined('APPLICATION_INNERPATH') || define('APPLICATION_INNERPATH', realpath( dirname(__FILE__) . '/../'));
+
+
+$PATH =  constant("APPLICATION_INNERPATH");
+
+require $PATH . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'service_config.php'; 
+$DB_CLASS =  $config_service['DB_CLASS'];
+require_once $config_service['FUNCTIONS'];
+require_once $DB_CLASS;
+
+
+?>
