d5a6818f200c19acb91d8c9555eb3bc4222d925d
Rasheed Ur Rehman <rasheed1612e@users.noreply.github.com>
2021-03-25 15:58:41 +0500

    save txt file

---
+++ app/controller/exportlist.php
@@ -0,0 +1,13 @@
+
+<?php
+              
+if(isset($_POST['submit']))
+{
+$data=$_POST['textdata'];
+$fp = fopen('data.txt', 'a');
+fwrite($fp, $data);
+fclose($fp);
+}
+?>
+
+ ?>
---
+++ app/controller/subscribers.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+defined('APPLICATION_INNERPATH') || define('APPLICATION_INNERPATH', realpath( dirname(__FILE__) . '/../'));
+
+
+$PATH =  constant("APPLICATION_INNERPATH");
+
+require $PATH . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'service_config.php'; 
+$DB_CLASS =  $config_service['DB_CLASS'];
+require_once $config_service['FUNCTIONS'];
+require_once $DB_CLASS;
+
+$query;
+$db;	
+$varr = new databaseManager();
+
+$email      = $_POST['email'] = isset($_POST['email']) ? $_POST['email'] : '';
+
+
+if(isset($email) && !empty($email)){
+
+     sendEmail($email);
+echo 1;
+
+  
+}
+?>
