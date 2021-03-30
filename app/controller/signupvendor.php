07446202f3f41874900cc2c1356a8380258b8e11
zubairayub <zubairayub@hotmail.com>
2021-03-30 18:31:04 +0500

    brand lang fix

--- app/controller/signupvendor.php
+++ app/controller/signupvendor.php
@@ -50,8 +50,8 @@ $DB_CLASS = $config_service['DB_CLASS'];
 		if (!empty($becomevendor)){
 		
 			$from_email = 'vendor@servewise.shop';
-			$message_body = '<h2>Welcome to ServeWise!</h2> <br><br>Your Request for become vendor has been sent. you will get status of your Shop <h4>' .$name. '</h4> with in 24 hours. Thanks for becoming a part of ServeWise';
-			 sendEmail($emailid,'ServeWise',$from_email,$message_body,'Become A Vendor');
+			$message_body = '<h2>Welcome to ServeWise!</h2> <br><br>Your Request for become Brand has been sent. you will get status of your Shop <h4>' .$name. '</h4> with in 24 hours. Thanks for becoming a part of ServeWise';
+			 sendEmail($emailid,'ServeWise',$from_email,$message_body,'Become A Brand');
 			
 			 insert_notifications($DB_CLASS,$userid,'6','vendor_singup','https://servewise.shop');
 			
