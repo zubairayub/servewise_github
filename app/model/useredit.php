9687acd5005fff2181390799b5327b2bc7fbfc6f
zubairayub <zubairayub@hotmail.com>
2021-06-16 18:31:06 +0500

    fix

--- app/model/useredit.php
+++ app/model/useredit.php
@@ -2,7 +2,7 @@
 	//ini_set('display_errors', 1);
 //ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);	
-session_start();
+//session_start();
 defined('APPLICATION_INNERPATH') || define('APPLICATION_INNERPATH', realpath( dirname(__FILE__) . '/../'));
 
 //echo 0;
