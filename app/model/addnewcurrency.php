c6e5b2421c2df9371e99d932223b55fa65c4127a
zubairayub <zubairayub@hotmail.com>
2021-06-17 19:32:48 +0500

    fixed currency n lang

--- app/model/addnewcurrency.php
+++ app/model/addnewcurrency.php
@@ -8,9 +8,6 @@ $update =  'false';
 if(isset($_GET['id'])){
 $id = $_GET['id'];
 $update =  'true';
-}
-
-
 
 $data = getcurrecny($id);	
 
@@ -21,4 +18,9 @@ $symbol = $data[0]['symbol'];
 $code = $data[0]['code'];
 $rate = $data[0]['rate'];
 }
+}
+
+
+
+
 ?>
